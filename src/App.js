51bc6f8a612cd09010ce2398b869741f01d28103
JR <minimucho1@gmail.com>
2018-12-15 16:03:21 -0600

    JR | Removing default content and adding Content component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header/header';
-import logo from './logo.svg';
+import Content from './Content/content';
 import './App.scss';
 
 class App extends Component {
@@ -8,20 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </div>
+        <Content />
       </div>
     );
   }
---
+++ src/Content/content.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './content.scss';
+
+// Will add routes in the future
+
+class Content extends Component {
+  render() {
+      return (
+        <main className="body">
+          <div className="body-container">
+            { null }
+          </div>
+        </main>
+      );
+  }
+}
+
+export default Content;
