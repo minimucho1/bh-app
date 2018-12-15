6a0e7ed0d7e863f9db6385d12d54b75ffc8c1a79
JR <minimucho1@gmail.com>
2018-12-15 16:09:51 -0600

    JR | Fixing broken reference to Polls in Content

--- src/Content/content.js
+++ src/Content/content.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 
-import Polls from './Polls/polls';
+import Polls from '../Polls/polls';
 import './content.scss';
 
 // Will add routes in the future
 
 class Content extends Component {
   render() {
-      return (
-        <main className="body">
-          <div className="body-container">
-            <Polls />
-          </div>
-        </main>
-      );
+    return (
+      <main className="body">
+        <div className="body-container">
+          <Polls />
+        </div>
+      </main>
+    );
   }
 }
 
