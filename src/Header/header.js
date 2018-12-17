0262b9465c36dff5e7dd8e63cea834b8932ad1aa
JR <minimucho1@gmail.com>
2018-12-16 20:43:19 -0600

    JR | Add 404 route

--- src/Header/header.js
+++ src/Header/header.js
@@ -10,8 +10,8 @@ class Header extends Component {
                         <nav className="header-container__nav">
                             <div className="header-container__nav-container">
                                     <button className="header-container__nav-container__item"><a href="/">Home</a></button>
-                                    <button className="header-container__nav-container__item"><a href="http://anination.info/Polls">Polls</a></button>
-                                    <button className="header-container__nav-container__item"><a href="http://anination.info/Polls">Results</a></button>
+                                    <button className="header-container__nav-container__item"><a href="http://anination.info/polls">Polls</a></button>
+                                    <button className="header-container__nav-container__item"><a href="http://anination.info/polls">Results</a></button>
                             </div>
                         </nav>
                 </div>
