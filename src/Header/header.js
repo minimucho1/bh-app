de80b7ab480d6c9db213c25bdb4fe95b0a2529df
JR <minimucho1@gmail.com>
2018-12-18 23:22:59 -0600

    JR | Clean up header.scss

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import { NavLink } from 'react-router-dom';
+
 import './header.scss';
 
-class Header extends Component {
-    render() {
-        return (
-            <header className="header">
-                <div className="header-container">
-                        <a className="header-container__logo" href="/"><img src={'images/anination-icon-70.png'} alt="logo" /></a>
-                        <nav className="header-container__nav">
-                            <div className="header-container__nav-container">
-                                <NavLink className="header-container__nav-container__item" exact to="/" activeClassName="selected">Home</NavLink>
-                                <NavLink className="header-container__nav-container__item" to="/polls" activeClassName="selected">Polls</NavLink>
-                                <NavLink className="header-container__nav-container__item" to="/results" activeClassName="selected">Results</NavLink>
-                            </div>
-                        </nav>
-                </div>
-            </header>
-        );
-    }
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-container grid-container">
+        <a className="header-container__logo grid-33" href="/"><img src={'images/anination-icon-70.png'} alt="logo" /></a>
+        <nav className="header-container__nav grid-66">
+          <div className="header-container__nav-container grid-container">
+            <NavLink className="header-container__nav-container__item grid-33" exact to="/" activeClassName="selected">Home</NavLink>
+            <NavLink className="header-container__nav-container__item grid-33" to="/polls" activeClassName="selected">Polls</NavLink>
+            <NavLink className="header-container__nav-container__item grid-33" to="/results" activeClassName="selected">Results</NavLink>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
